685d20c802e8acb418c1804a7fc1f2a6483fc662
Roseak-lin <roseak.lin@gmail.com>
2021-07-16 11:11:19 -0400

    Shortened code
    Made code simpler

--- Phonebook.java
+++ Phonebook.java
@@ -22,6 +22,7 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Phonebook extends Application {
+	final String FILE_PATH = "phonebook.txt";
 	String fName, lName, email, school, number;
 	Text t = new Text();
 	HashMap<Integer, String> entries = new HashMap<Integer, String>();
@@ -30,16 +31,21 @@ public class Phonebook extends Application {
 	boolean findButtonClick = true, arrowButtonClick = true;;
 	String previous;
 
+	Button submit = new Button("Submit");
+	Button clear = new Button("Clear");
+	Button find = new Button("Find account By First Name");
+	Button update = new Button("Update");
+
 	public static void main(String[] args) {
 		launch("Phonebook");
 
 	}
 
 	public void start(Stage primaryStage) {
-		// setup ///////////////////////////////////////
+		//// set up ////
 		Group group = new Group();
 		Scene scene = new Scene(group);
-		primaryStage.setHeight(300);
+		primaryStage.setHeight(350);
 		primaryStage.setWidth(375);
 		primaryStage.setTitle("Phonebook");
 		primaryStage.show();
@@ -60,9 +66,9 @@ public class Phonebook extends Application {
 		t2.setLayoutX(100);
 		t2.setLayoutY(30);
 
-		Text e = new Text("E-mail Address");
-		e.setLayoutX(5);
-		e.setLayoutY(78);
+		Text em = new Text("E-mail Address");
+		em.setLayoutX(5);
+		em.setLayoutY(78);
 
 		TextField t3 = new TextField();
 		t3.setLayoutX(100);
@@ -84,38 +90,40 @@ public class Phonebook extends Application {
 		t5.setLayoutX(100);
 		t5.setLayoutY(120);
 
-		File left = new File("left_arrow.png");
-		File right = new File("right_arrow.png");
+		t.setVisible(false);
+		t.setX(100);
+		t.setY(165);
+
+		File left = new File("C:\\Users\\rosea\\Pictures\\Java\\arrow1.png");
+		File right = new File("C:\\Users\\rosea\\Pictures\\Java\\arrow2.png");
 
 		Image l1 = new Image(left.toURI().toString(), 20, 20, false, false);
 		Image r = new Image(right.toURI().toString(), 20, 20, false, false);
 
-		// arrow keys ///////////////////////////////////////////
+		///// Arrow Keys /////
 		Button leftA = new Button();
 		leftA.setGraphic(new ImageView(l1));
 		leftA.setLayoutX(275);
 		leftA.setLayoutY(150);
 		leftA.setVisible(false);
-		leftA.setOnAction(new EventHandler<ActionEvent>() {
-			public void handle(ActionEvent event) {
-				if (currentAccount == 0) {
-					currentAccount--;
-					currentAccount = arr.size() - 1;
-					t1.setText(entries.get(arr.get(currentAccount)));
-					t2.setText(entries.get(arr.get(currentAccount) + 1));
-					t3.setText(entries.get(arr.get(currentAccount) + 2));
-					t4.setText(entries.get(arr.get(currentAccount) + 3));
-					t5.setText(entries.get(arr.get(currentAccount) + 4));
-				} else {
-					currentAccount--;
-					t1.setText(entries.get(arr.get(currentAccount)));
-					t2.setText(entries.get(arr.get(currentAccount) + 1));
-					t3.setText(entries.get(arr.get(currentAccount) + 2));
-					t4.setText(entries.get(arr.get(currentAccount) + 3));
-					t5.setText(entries.get(arr.get(currentAccount) + 4));
-				}
-
+		leftA.setOnAction(event -> {
+			if (currentAccount == 0) {
+				currentAccount--;
+				currentAccount = arr.size() - 1;
+				t1.setText(entries.get(arr.get(currentAccount)));
+				t2.setText(entries.get(arr.get(currentAccount) + 1));
+				t3.setText(entries.get(arr.get(currentAccount) + 2));
+				t4.setText(entries.get(arr.get(currentAccount) + 3));
+				t5.setText(entries.get(arr.get(currentAccount) + 4));
+			} else {
+				currentAccount--;
+				t1.setText(entries.get(arr.get(currentAccount)));
+				t2.setText(entries.get(arr.get(currentAccount) + 1));
+				t3.setText(entries.get(arr.get(currentAccount) + 2));
+				t4.setText(entries.get(arr.get(currentAccount) + 3));
+				t5.setText(entries.get(arr.get(currentAccount) + 4));
 			}
+
 		});
 
 		Button rightA = new Button();
@@ -123,75 +131,133 @@ public class Phonebook extends Application {
 		rightA.setLayoutX(315);
 		rightA.setLayoutY(150);
 		rightA.setVisible(false);
-		rightA.setOnAction(new EventHandler<ActionEvent>() {
-			public void handle(ActionEvent event) {
-				if (currentAccount == arr.size() - 1) {
-					currentAccount = 0;
-					t1.setText(entries.get(arr.get(currentAccount)));
-					t2.setText(entries.get(arr.get(currentAccount) + 1));
-					t3.setText(entries.get(arr.get(currentAccount) + 2));
-					t4.setText(entries.get(arr.get(currentAccount) + 3));
-					t5.setText(entries.get(arr.get(currentAccount) + 4));
-				} else {
-					currentAccount++;
-					t1.setText(entries.get(arr.get(currentAccount)));
-					t2.setText(entries.get(arr.get(currentAccount) + 1));
-					t3.setText(entries.get(arr.get(currentAccount) + 2));
-					t4.setText(entries.get(arr.get(currentAccount) + 3));
-					t5.setText(entries.get(arr.get(currentAccount) + 4));
-				}
+		rightA.setOnAction(event -> {
+			if (currentAccount == arr.size() - 1) {
+				currentAccount = 0;
+				t1.setText(entries.get(arr.get(currentAccount)));
+				t2.setText(entries.get(arr.get(currentAccount) + 1));
+				t3.setText(entries.get(arr.get(currentAccount) + 2));
+				t4.setText(entries.get(arr.get(currentAccount) + 3));
+				t5.setText(entries.get(arr.get(currentAccount) + 4));
+			} else {
+				currentAccount++;
+				t1.setText(entries.get(arr.get(currentAccount)));
+				t2.setText(entries.get(arr.get(currentAccount) + 1));
+				t3.setText(entries.get(arr.get(currentAccount) + 2));
+				t4.setText(entries.get(arr.get(currentAccount) + 3));
+				t5.setText(entries.get(arr.get(currentAccount) + 4));
 			}
 		});
 
-		// main buttons and functions //////////////////////
-		Button submit = new Button("Submit");
+		///// main buttons and functions /////
+
 		submit.setLayoutX(5);
-		submit.setLayoutY(150);
-		submit.setOnAction(new EventHandler<ActionEvent>() {
-			public void handle(ActionEvent event) {
-				try {
-					fName = t1.getText();
-					lName = t2.getText();
-					email = t3.getText();
-					number = t4.getText();
-					school = t5.getText();
+		submit.setLayoutY(175);
+		submit.setOnAction(event -> {
+			try {
+				fName = t1.getText();
+				lName = t2.getText();
+				email = t3.getText();
+				number = t4.getText();
+				school = t5.getText();
 
-					FileWriter fw = new FileWriter("phonebook.txt", true);
-					BufferedWriter bw = new BufferedWriter(fw);
-					bw.write("First Name: " + fName);
-					bw.newLine();
-					bw.write("Last Name: " + lName);
-					bw.newLine();
-					bw.write("E-mail Address: " + email);
-					bw.newLine();
-					bw.write("Phone Number: " + number);
-					bw.newLine();
-					bw.write("School: " + school);
-					bw.newLine();
-					bw.write(" ");
-					bw.newLine();
-					bw.close();
-					fw.close();
-
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+				FileWriter fw = new FileWriter(FILE_PATH, true);
+				BufferedWriter bw = new BufferedWriter(fw);
+				bw.write("First Name: " + fName);
+				bw.newLine();
+				bw.write("Last Name: " + lName);
+				bw.newLine();
+				bw.write("E-mail Address: " + email);
+				bw.newLine();
+				bw.write("Phone Number: " + number);
+				bw.newLine();
+				bw.write("School: " + school);
+				bw.newLine();
+				bw.write("");
+				bw.newLine();
+				bw.close();
+				fw.close();
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 		});
 
-		t.setVisible(false);
-		t.setX(100);
-		t.setY(165);
+		find.setLayoutX(5);
+		find.setLayoutY(205);
 
-		Button update = new Button("Update");
+		find.setOnAction(event -> {
+			try {
+				arr.clear();
+				int totalLines = 0;
+				currentAccount = 0;
+				int count = 0;
+				BufferedReader br = new BufferedReader(
+						new FileReader(FILE_PATH));
+				String line;
+				while ((line = br.readLine()) != null) {
+					String[] parts = line.split(": ");
+					if (parts.length > 1) {
+						if (parts[0].equals("First Name") && parts[1].equals(t1.getText())) {
+							arr.add(totalLines);
+							count++;
+						}
+						entries.put(totalLines, parts[1]);
+						totalLines++;
+					}
+				}
+
+				br = new BufferedReader(new FileReader(FILE_PATH));
+				if (count == 1) {
+					leftA.setVisible(false);
+					rightA.setVisible(false);
+					t1.setText(entries.get(arr.get(0)));
+					t2.setText(entries.get(arr.get(0) + 1));
+					t3.setText(entries.get(arr.get(0) + 2));
+					t4.setText(entries.get(arr.get(0) + 3));
+					t5.setText(entries.get(arr.get(0) + 4));
+					t.setText("");
+					while (!br.readLine().equals("First Name: " + t1.getText())) {
+						continue;
+					}
+					update.setVisible(true);
+				} else if (count > 1) {
+					t.setText("There were " + count + " accounts found");
+					update.setVisible(true);
+					t.setVisible(true);
+					leftA.setVisible(true);
+					rightA.setVisible(true);
+				} else {
+					t.setText("There were no accounts found");
+					t.setVisible(true);
+					leftA.setVisible(false);
+					rightA.setVisible(false);
+					update.setVisible(false);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		});
+
+		clear.setLayoutX(5);
+		clear.setLayoutY(235);
+
+		clear.setOnAction(event -> {
+			t1.clear();
+			t2.clear();
+			t3.clear();
+			t4.clear();
+			t5.clear();
+		});
+
 		update.setLayoutX(5);
-		update.setLayoutY(225);
-		update.setVisible(false);
+		update.setLayoutY(265);
+		update.setVisible(true);
 
 		update.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent event) {
 				try {
-					FileWriter fw = new FileWriter("phonebook.txt");
+					FileWriter fw = new FileWriter(FILE_PATH);
 					BufferedWriter bw = new BufferedWriter(fw);
 					try {
 						entries.replace(arr.get(currentAccount), t1.getText());
@@ -225,99 +291,11 @@ public class Phonebook extends Application {
 			}
 		});
 
-		Button find = new Button("Find account By First Name");
-		find.setLayoutX(5);
-		find.setLayoutY(200);
-
-		find.setOnAction(new EventHandler<ActionEvent>() {
-			public void handle(ActionEvent event) {
-				try {
-					int totalLines = 0;
-					currentAccount = 0;
-					int count = 0;
-					BufferedReader br = new BufferedReader(new FileReader("phonebook.txt"));
-					if (findButtonClick) {
-						findButtonClick = false;
-						String line;
-						while ((line = br.readLine()) != null) {
-							String[] parts = line.split(": ");
-							if (parts.length > 1) {
-								entries.put(totalLines, parts[1]);
-								if (parts[0].equals("First Name") && parts[1].equals(t1.getText())) {
-									arr.add(totalLines);
-								}
-								totalLines++;
-							} else {
-								continue;
-							}
-						}
-					}
-					br = new BufferedReader(new FileReader("phonebook.txt"));
-					for (int i = 0; i < totalLines; i++) {
-						String temp1 = br.readLine();
-						if (("First Name: " + t1.getText()).equals(temp1)) {
-							count++;
-						}
-						temp1 = br.readLine();
-					}
-					int temp1 = 0;
-					while (!t1.getText().equals(entries.get(temp1)) && temp1 < entries.size()) {
-						temp1++;
-					}
-					br = new BufferedReader(new FileReader("phonebook.txt"));
-					if (count == 1) {
-						leftA.setVisible(false);
-						rightA.setVisible(false);
-						t1.setText(entries.get(temp1));
-						t2.setText(entries.get(temp1 + 1));
-						t3.setText(entries.get(temp1 + 2));
-						t4.setText(entries.get(temp1 + 3));
-						t5.setText(entries.get(temp1 + 4));
-						t.setText("");
-						while (!br.readLine().equals("First Name: " + t1.getText())) {
-							continue;
-						}
-						update.setVisible(true);
-					} else if (count > 1) {
-						t.setText("There were " + count + " accounts found");
-						update.setVisible(true);
-						t.setVisible(true);
-						leftA.setVisible(true);
-						rightA.setVisible(true);
-					} else {
-						t.setText("There were no accounts found");
-						t.setVisible(true);
-						leftA.setVisible(false);
-						rightA.setVisible(false);
-						update.setVisible(false);
-					}
-					count = 0;
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-
-			}
-		});
-
-		Button clear = new Button("Clear");
-		clear.setLayoutX(5);
-		clear.setLayoutY(175);
-
-		clear.setOnAction(new EventHandler<ActionEvent>() {
-			public void handle(ActionEvent event) {
-				t1.clear();
-				t2.clear();
-				t3.clear();
-				t4.clear();
-				t5.clear();
-			}
-		});
-
 		group.getChildren().add(f);
 		group.getChildren().add(t1);
 		group.getChildren().add(l);
 		group.getChildren().add(t2);
-		group.getChildren().add(e);
+		group.getChildren().add(em);
 		group.getChildren().add(t3);
 		group.getChildren().add(num);
 		group.getChildren().add(t4);
